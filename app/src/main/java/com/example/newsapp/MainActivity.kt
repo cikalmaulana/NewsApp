41fe088cc646b515131fe79951ead42bb3c2cad0
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2023-05-31 09:48:24 +0700

    Edit MainActivity & RecyclerView
    + Add card view for headline
    + Styling RecyclerView, CardView

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.example.newsapp.network.NewsApiService
 import kotlinx.coroutines.CoroutineScope
@@ -12,6 +13,9 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,11 +49,25 @@ class MainActivity : AppCompatActivity() {
                 val newsList = ArrayList(newsResponse?.articles ?: emptyList())
                 withContext(Dispatchers.Main) {
                     binding.rvNews.adapter = NewsAdapter(newsList)
+                    Glide.with(binding.root)
+                        .load(newsList.get(0).urlToImage)
+                        .into(binding.ivHeadline)
+                    binding.tvHeadline.text = newsList.get(0).title
+                    binding.tvHeadlineAuthor.text = newsList.get(0).author
+                    binding.tvHeadlineDate.text = changeDateFormat(newsList.get(0).publishedAt)
                 }
             } else {
                 val error = response.errorBody()?.string()
                 Log.d("HIT_NEWS", "Gagal Fetch Data")
             }
         }
+    }
+
+    fun changeDateFormat(inputDate: String): String {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("d MMM, yyyy", Locale.getDefault())
+
+        val date = inputFormat.parse(inputDate)
+        return outputFormat.format(date)
     }
 }
--- app/src/main/java/com/example/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -5,6 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newsapp.databinding.ItemNewsBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class NewsAdapter(private val news : ArrayList<NewsItem>) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
@@ -21,10 +24,20 @@ class NewsAdapter(private val news : ArrayList<NewsItem>) :
         fun bind(news: NewsItem) {
             binding.tvTitle.text = news.title
             binding.tvSubtitle1.text = news.author
-            binding.tvSubtitle2.text = news.publishedAt
+            binding.tvSubtitle2.text = changeDateFormat(news.publishedAt)
             Glide.with(binding.root)
                 .load(news.urlToImage)
                 .into(binding.ivIcon)
         }
+
+        fun changeDateFormat(inputDate: String): String {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("d MMM, yyyy", Locale.getDefault())
+
+            val date = inputFormat.parse(inputDate)
+            return outputFormat.format(date)
+        }
     }
+
+
 }
